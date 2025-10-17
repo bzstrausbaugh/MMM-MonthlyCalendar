8ac8eab1a8ba4cd23658e5eed30980152e2b04f4
Brant Strausbaugh <brant.strausbaugh@projectroseland.com>
2025-10-17 15:02:29 -0500

    Do not blank out selected date

--- MMM-MonthlyCalendar.js
+++ MMM-MonthlyCalendar.js
@@ -229,7 +229,7 @@ Module.register('MMM-MonthlyCalendar', {
           self.updateDom();
         }
       }, 5000);
-      setDetailContent(self.event);
+      setDetailContent(self.event, self.selectedDate);
     }
   },
 
